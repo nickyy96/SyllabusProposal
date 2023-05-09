a2d92c5c5eff068430be36eb68cf0143489e6c4d
nickyy96 <nick.yarnall9@gmail.com>
2023-05-09 02:50:44 -0400

    FEAT: done with site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,84 @@
 import Image from 'next/image'
+'use client';
+
+import { useState } from 'react'
+
+;
 
 export default function Home() {
+  const [mount, setMount] = useState(true)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex min-h-screen max-h-screen overflow-hidden items-center flex-col bg-blue-300 relative">
+      {
+        mount && 
+        <div className='absolute top-0 left-0 right-0 bottom-0 bg-black bg-opacity-80 flex items-center justify-center backdrop-blur-sm' onClick={() => setMount(false)}>
+          <div className='bg-gray-950 flex flex-col max-w-[40vw] min-h-[50vh] rounded-3xl relative'>
+            <div className='border-b-[1px] text-white p-4 flex items-center justify-center text-lg'>
+              <p><strong>Welcome</strong> to my final project 🙏🤖</p>
+            </div>
+            <div className='w-full flex flex-col items-center text-white p-4'>
+              <p className=''>The intersection of religion and technology is an area that should be included in the syllabus. On 
+              this webpage, I will showcase some amazing examples of <strong>AI driven style transfer</strong>. By leveraging state of the art image recognition
+              models and ideas about temporal consistency loss, I was able to create a model that can transfer the style of an input image to a video or picture.</p>
+              <br></br>
+              <p>I believe this kind of project is an interesting example of how <strong>AI could reasonably facilitate the expansion of religion to new generations</strong> by generating 
+              content which is more interesting to children in a far more efficient manner.</p>
+              <br></br>
+              <p>To interact with this application click this link to my code, download the github repository, and follow the instructions in the README!
+              </p>
+              <br></br>
+              <a href="https://github.com/nickyy96/sequence-stylizers" target="_blank" rel="noopener noreferrer" >
+                <strong>GitHub</strong>
+              </a>
+              <br></br>
+              <p>I worked on this project for Computer Vision at Brown, and then added an interface and generated images for this project! 😄😄</p>
+            </div>
+            <div className='text-blue-200 flex items-center justify-center p-2 border-t-[1px]' onClick={(e) => e.stopPropagation()}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-chevron-down cursor-pointer" viewBox="0 0 16 16" onClick={(e) => {
+                e.stopPropagation()
+                setMount(false)
+              }}>
+                <path fillRule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </div>
+          </div>
         </div>
+      }
+
+
+      <div className="flex w-full min-h-[8vh] p-2 text-white items-center justify-between">
+        <p className="font-bold w-1/3">RELS 0815 <span className="font-normal">final project</span></p>
+        <p className='font-bold text-[1.5rem] w-1/3 flex justify-center hover:text-[1.7rem] transition-[font-size] ease-in-out cursor-pointer' onClick={() => setMount(true)}>Style Transfer Exhibit</p>
+        <strong className='w-1/3 flex justify-end'>Nicholas Yarnall</strong>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='flex flex-grow min-w-full min-h-full border-t-[1px] items-center justify-center bg-blue-200'>
+        <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)', gridTemplateRows: 'repeat(2, 1fr)', gap: '10px' }} className="max-w-[80vw] max-h-[70vh]">
+          <div className='flex flex-col items-center'>
+            <img src="/christ.png" alt="slide 1" className='hover:opacity-90 border-[1px] border-white'/>
+            <p>Jesus Christ</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src="/books.png" alt="slide 2" className='hover:opacity-90 border-[1px] border-white'/>
+            <p>The Quran</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src="/conficius.png" alt="slide 3" className='hover:opacity-90 border-[1px] border-white'/>
+            <p>Confucius</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src="/last_supper.png" alt="slide 4" className='hover:opacity-90 border-[1px] border-white'/>
+            <p>The Last Supper</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src="/temple.png" alt="slide 5" className='hover:opacity-90 border-[1px] border-white'/>
+            <p>A Synagogue</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <img src="/temple2.png" alt="slide 6" className='hover:opacity-90 border-[1px] border-white'/>
+            <p>A Temple</p>
+          </div>
+        </div>
       </div>
     </main>
   )
